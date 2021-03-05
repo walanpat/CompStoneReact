e44de830dbcc6279d0531b8f0cd095afd3114f99
walanpat <39136284+walanpat@users.noreply.github.com>
2021-03-05 15:42:00 -0500

    Formatting Push/Turnery Introduction

--- my-app/src/pages/cardwiki/Wiki.js
+++ my-app/src/pages/cardwiki/Wiki.js
@@ -1,9 +1,10 @@
 import React from "react";
+import cardData from "../../engine/Cards";
 
 const Wiki = () => {
   return (
     <div>
-      
+      {cardData!==null?cardData.map(e =>(e.cardimg)):null}
     </div>
   );
 };
